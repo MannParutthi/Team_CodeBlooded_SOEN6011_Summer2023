40c06758fd1cdf53a36ca38c09a570d2d8153073
Ashwin Raghunath <ashwinr2296@gmail.com>
2023-07-17 11:16:59 -0400

    US-09 Employer profile endpoints added

---
+++ career-service-backend/src/main/java/com/soen6011/careerservicebackend/response/EmployerProfileResponse.java
@@ -0,0 +1,11 @@
+package com.soen6011.careerservicebackend.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployerProfileResponse {
+    private String companyName;
+    private String website;
+}
