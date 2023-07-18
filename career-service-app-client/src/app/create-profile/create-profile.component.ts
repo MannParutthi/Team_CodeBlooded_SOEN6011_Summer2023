aa3e2723c305990b5a0e85fa39638081eabffffa
jwalit21 <jwalitshah2q@gmail.com>
2023-07-18 14:08:37 -0400

    Update profile added

--- career-service-app-client/src/app/create-profile/create-profile.component.ts
+++ career-service-app-client/src/app/create-profile/create-profile.component.ts
@@ -4,6 +4,7 @@ import { CreateProfileService } from './create-profile.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { BrowseJobsService } from '../browse-jobs/browse-jobs.service';
+import { Authority } from 'src/constants';
 
 @Component({
   selector: 'app-create-profile',
@@ -12,11 +13,16 @@ import { BrowseJobsService } from '../browse-jobs/browse-jobs.service';
 })
 export class CreateProfileComponent implements OnInit {
 
-  profileForm: FormGroup = this.formBuilder.group({
+  candidateProfileForm: FormGroup = this.formBuilder.group({
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    phoneNumber: ['', Validators.required, Validators.pattern('^[0-9]*$')]
+    education: ['', Validators.required],
+    experience: [0, Validators.required]
+  });
+
+  employerProfileForm: FormGroup = this.formBuilder.group({
+    companyName: ['', Validators.required],
+    website: ['', Validators.required]
   });
 
   selectedFile: File | null = null;
@@ -24,22 +30,38 @@ export class CreateProfileComponent implements OnInit {
   loggedUser: any;
   resumeId: any;
   resumeExists: any;
+  Authority: any;
   constructor(private formBuilder: FormBuilder, private createProfileService: CreateProfileService, private browseJobsService: BrowseJobsService, private _router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     // Check if the user is already logged in, if so, navigate to the home page
+    this.Authority = Authority
     this.loggedUser = localStorage.getItem('user')
     this.loggedUser = JSON.parse(this.loggedUser)
     if (this.loggedUser == null) {
       this._router.navigateByUrl('/home');
     }
+    if (this.loggedUser.authority === Authority.CANDIDATE) {
+      this.candidateProfileForm.setValue({
+        firstName: this.loggedUser.firstName,
+        lastName: this.loggedUser.lastName,
+        education: this.loggedUser.education,
+        experience: this.loggedUser.experience
+      })
+    }
+    if (this.loggedUser.authority === Authority.EMPLOYER) {
+      this.employerProfileForm.setValue({
+        companyName: this.loggedUser.companyName,
+        website: this.loggedUser.website
+      })
+    }
+
     this.browseJobsService.isResumeExists(this.loggedUser.userId).subscribe(
       (res: any) => {
         this.resumeExists = res
         if (this.resumeExists) {
           this.createProfileService.downloadResume(this.loggedUser.userId).subscribe(
             (res: any) => {
-              console.log(res)
               let url = window.URL.createObjectURL(res);
               let a = document.getElementById('downloadButton');
               if (a instanceof HTMLAnchorElement) {
@@ -48,22 +70,49 @@ export class CreateProfileComponent implements OnInit {
               }
             },
             (error: any) => { 
-              console.error('File download failed', error);
+              this.toastr.error('Resume download failed', 'Download Failed');
             }
           );
         }
       },
       (error: any) => { 
-        console.error('API call failed', error);
-        return
+        this.toastr.error('Error occured', 'Failed');
       }
     );
   }
 
-  createProfile() {
-    if (this.profileForm.valid) {
-      console.log(this.profileForm.value);
-      // Perform further actions (e.g., save to backend)
+  updateProfile() {
+    if (this.loggedUser.authority === Authority.CANDIDATE && this.candidateProfileForm.valid) {
+      console.log(this.candidateProfileForm.value);
+      this.createProfileService.updateCandidateProfile(this.loggedUser.userId, this.candidateProfileForm.getRawValue()).subscribe(
+        (res: any) => {
+          this.loggedUser.firstName = res.firstName
+          this.loggedUser.lastName = res.lastName
+          this.loggedUser.experience = res.experience
+          this.loggedUser.education = res.education
+          localStorage.setItem("user", JSON.stringify(this.loggedUser))
+          this.toastr.success('Updated candidate profile successfully', 'Update successfull');
+          this._router.navigateByUrl('/my-profile')
+        },
+        (error: any) => { 
+          this.toastr.error('Update candidate profile Failed', 'Update Failed');
+        }
+      );
+    }
+    if (this.loggedUser.authority === Authority.EMPLOYER && this.employerProfileForm.valid) {
+      console.log(this.employerProfileForm.value);
+      this.createProfileService.updateEmployerProfile(this.loggedUser.userId, this.employerProfileForm.getRawValue()).subscribe(
+        (res: any) => {
+          this.loggedUser.companyName = res.companyName
+          this.loggedUser.website = res.website
+          localStorage.setItem("user", JSON.stringify(this.loggedUser))
+          this.toastr.success('Updated employer profile successfully', 'Update successfull');
+          this._router.navigateByUrl('/my-profile')
+        },
+        (error: any) => { 
+          this.toastr.error('Update employer profile Failed', 'Update Failed');
+        }
+      );
     }
   }
 
