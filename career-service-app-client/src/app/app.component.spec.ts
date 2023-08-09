3990be6b91720bb2b18f40e83138d09384c56f52
nipunh <nipunhedaoo@gmail.com>
2023-08-09 18:23:34 -0400

    Update app.component.spec.ts

--- career-service-app-client/src/app/app.component.spec.ts
+++ career-service-app-client/src/app/app.component.spec.ts
@@ -1,35 +1,47 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'career-service-app-client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('career-service-app-client');
+  it('should set loggedUser and Authority in ngOnInit', () => {
+    const userData = { username: 'testuser', role: 'user' };
+    localStorage.setItem('user', JSON.stringify(userData));
+
+    component.ngOnInit();
+
+    expect(component.loggedUser).toEqual(userData);
+    expect(component.Authority).toBeDefined();
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('career-service-app-client app is running!');
-  // });
+  it('should clear local storage and navigate to home on logout', () => {
+    spyOn(localStorage, 'clear');
+    spyOn(component._router, 'navigateByUrl');
+    spyOn(window.location, 'reload');
+
+    component.logout();
+
+    expect(localStorage.clear).toHaveBeenCalled();
+    expect(component._router.navigateByUrl).toHaveBeenCalledWith('/home');
+    expect(window.location.reload).toHaveBeenCalled();
+  });
 });
